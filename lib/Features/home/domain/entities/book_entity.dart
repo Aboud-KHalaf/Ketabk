ac156b3a5a1760375cdf23b0801712d64f7f6cdb
A B O U D <aboudkalaf10@gmail.com>
2024-08-06 14:03:04 +0300

    create book entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,18 @@
+class BookEntity {
+  final String image;
+  final String titlel;
+  final String atutherName;
+  final num price;
+  final num rating;
+
+  BookEntity({
+    required this.image,
+    required this.titlel,
+    required this.atutherName,
+    required this.price,
+    required this.rating,
+  });
+}
+
+// notes : 
+// 1- "num" uses for double and integer numbers.
