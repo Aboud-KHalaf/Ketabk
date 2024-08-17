ba7d98f587fa23d7b075461f45c304cc6101fbea
A B O U D <aboudkalaf10@gmail.com>
2024-08-17 18:20:04 +0300

    newest books cubit done

---
+++ lib/Features/home/presentation/manager/cubit/newest_books_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_newest_books_usecase.dart';
+import 'package:meta/meta.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.fetchNewestBooksUsecase) : super(NewestBooksInitial());
+
+  final FetchNewestBooksUsecase fetchNewestBooksUsecase;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksInitial());
+    var result = await fetchNewestBooksUsecase.call();
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(errMessage: failure.message));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books: books));
+      },
+    );
+  }
+}
